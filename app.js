5774db3f35e31b4f2674867f8c40c11ffaa4a8ba
JasanCarsley <jjcars@comcast.net>
2020-09-29 17:31:12 -0700

    added additional button functionality

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ import { compareNumbers, getRandomNumber } from './utils.js';
 const button = document.getElementById('submit-button');
 const input = document.getElementById('number-input');
 const guessesRemaining = document.getElementById('guesses-remaining');
-const numberGuessed = document.getElementById('number-guessed');
+
 const resultMessage = document.getElementById('result-message');
 // initialize state
 let guessCounter = 10;
+guessesRemaining.textContent = guessCounter; 
 
 const randomNumber = getRandomNumber();
+console.log(randomNumber);
 // set event listeners to update state and DOM
 
 button.addEventListener('click', () => {
@@ -18,8 +20,22 @@ button.addEventListener('click', () => {
     console.log(userGuess);
 
     if (compareResult === 0) {
-
+        resultMessage.textContent = 'You are correct!';
+    } if (compareResult === -1) {
+        resultMessage.textContent = 'Your guess is too low!';
     }
+    if (compareResult === 1)
+    { resultMessage.textContent = 'Your guess is too high!'; }
+
+    guessCounter--;
+    guessesRemaining.textContent = guessCounter;
+
+    if (guessCounter === 0) {
+        resultMessage.textContent = 'Game over!';
+    }
+
+    
+
 });
 
 
